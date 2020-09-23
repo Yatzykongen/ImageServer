e5a702139a5e95e8196de4e029f28347ce664bc5
Sondre Nerhus <sondre.nerhus@hotmail.com>
2020-09-23 10:47:17 +0200

    Manage to make a class that sends images with and without a brake.

--- src/ImageHandler.java
+++ src/ImageHandler.java
@@ -1,87 +1,51 @@
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.nio.Buffer;
-import java.nio.ByteBuffer;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
 
-/**
- * @author sonrdrrrrre
- */
-public class ImageHandler implements Runnable
+public class ImageHandler
 {
-    private Socket client;
-    private BufferedInputStream imageStream;
-    private BufferedReader in;
-    private PrintWriter out;
-    private InputStream inputStream;
-    private FileOutputStream fileOutputStream;
-
-    public ImageHandler(Socket imageSocket) throws IOException
+    public static void main(String[] args) throws IOException
     {
-        this.client = imageSocket;
-        imageStream = new BufferedInputStream(client.getInputStream());
-        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
-        out = new PrintWriter(client.getOutputStream());
-        inputStream = client.getInputStream();
-        //fileOutputStream = new FileOutputStream();
-        System.out.println("den ble laget");
+        ServerSocket serverSocket = new ServerSocket(42069);
+
+        Socket socket = serverSocket.accept();
+        System.out.println("Connected to client!");
+
+        InputStream inputStream = socket.getInputStream();
+        ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
+        int imageCount = 0;
+        ImageObject image = null;
+
+
+        while (imageCount < 10) {
+            try
+            {
+                image = (ImageObject) objectInputStream.readObject();
+                if (!(image.equals(null)) & image.getSize()>20000)
+                {
+                    byteArrayToImage(image.getImageBytes(), image.getFiletype(), image.getName());
+
+                    image = null;
+                    System.out.println("Image was made!");
+                    imageCount ++;
+                }
+            }
+            catch (ClassNotFoundException e)
+            {
+                System.out.println(e.getMessage());
+            }
+        }
+        socket.close();
     }
 
-    @Override
-    public void run()
+    private static void byteArrayToImage(byte[] imageBytes, String filetype, String name) throws IOException
     {
-        // GUSTAV TESTER GIT PÅ STASJA
-        try
-        {
-            int imageCounter = 0;
-            while(!client.isClosed() & imageCounter<3)
-            {
-                //do something
-//                byte[] sizeAr = new byte[4];
-//                inputStream.read(sizeAr);
-//                int size = ByteBuffer.wrap(sizeAr).asIntBuffer().get();
-//                System.out.println(size);
-//
-//                byte[] imageAr = new byte[size];
-//                inputStream.read(imageAr);
-//                System.out.println("fekk bilde");
-//
-//                BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(imageAr));
-
-
-//                ImageIO.write(bufferedImage, "jpg", new File("C:\\Users\\sondr\\Pictures\\Prosjekt\\image"+imageCounter+".jpg"));
-//                System.out.println(imageCounter);
-//                imageCounter++;
-//                Thread.sleep(11000);
-            System.out.println("Loading image");
-            //BufferedInputStream imageStream = new BufferedInputStream(client.getInputStream());
-            BufferedImage bufferedImage = ImageIO.read(imageStream);
-            if(bufferedImage != (null))
-            {
-                System.out.println("Saving image");
-                ImageIO.write(bufferedImage, "jpg", new File("C:\\Users\\sondr\\Pictures\\Prosjekt\\image" + imageCounter + ".jpg"));
-                System.out.println("Image saved");
-                imageCounter++;
-                //imageStream.close();
-            }
-
-
-            }
-            imageStream.close();
-            client.close();
-        }
-        catch (IOException e)
-        {
-            System.err.println(e.getStackTrace());
-        }
-//        catch (InterruptedException e)
-//        {
-//
-//        }
-
+        ByteArrayInputStream bis = new ByteArrayInputStream(imageBytes);
+        BufferedImage bImage2 = ImageIO.read(bis);
+        ImageIO.write(bImage2, filetype, new File("C:\\Users\\sondr\\Pictures\\Prosjekt\\"+name+"."+filetype) );
+        System.out.println("image created");
     }
 }
